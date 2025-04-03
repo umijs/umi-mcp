e368eef3b804881a49be31ec58ebd2417bf5579c
Wu-kung <1434246346@qq.com>
2025-04-03 11:14:45 +0800

    feat: add umi-config,umi-build,umi-version

---
+++ src/tools/index.ts
@@ -0,0 +1,4 @@
+export { default as umiHelp } from './umi-help';
+export { default as umiConfig } from './umi-config';
+export { default as umiBuild } from './umi-build';
+export { default as umiVersion } from './umi-version';
---
+++ src/tools/umi-config.ts
@@ -0,0 +1,59 @@
+import { execSync } from 'child_process';
+import { z } from 'zod';
+import { parse } from '../parse';
+import { ToolContext } from '../types';
+
+const umiConfig = async ({ server, root }: ToolContext) => {
+  server.addTool({
+    name: 'umi-config-list',
+    description: 'List all available umi config',
+    parameters: z.object({}),
+    execute: async () => {
+      const { binPath } = parse(root);
+      const result = execSync(`${binPath} config list`);
+      return result.toString();
+    },
+  });
+
+  server.addTool({
+    name: 'umi-config-get',
+    description: 'Get the value of a config of the umi project',
+    parameters: z.object({
+      key: z.string(),
+    }),
+    execute: async ({ key }) => {
+      const { binPath } = parse(root);
+      const result = execSync(`${binPath} config get ${key}`);
+      return result.toString();
+    },
+  });
+
+  server.addTool({
+    name: 'umi-config-set',
+    description: 'Set the value of a config of the umi project',
+    parameters: z.object({
+      key: z.string(),
+      value: z.string(),
+    }),
+    execute: async ({ key, value }) => {
+      const { binPath } = parse(root);
+      const result = execSync(`${binPath} config set ${key} ${value}`);
+      return result.toString();
+    },
+  });
+
+  server.addTool({
+    name: 'umi-config-remove',
+    description: 'Remove a config of the umi project',
+    parameters: z.object({
+      key: z.string(),
+    }),
+    execute: async ({ key }) => {
+      const { binPath } = parse(root);
+      const result = execSync(`${binPath} config remove ${key}`);
+      return result.toString();
+    },
+  });
+};
+
+export default umiConfig;
