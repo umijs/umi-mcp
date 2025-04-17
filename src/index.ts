1cfaacfa85ffd7e98457abac71d54d29bcec1c26
Wu-kung <1434246346@qq.com>
2025-04-17 17:57:51 +0800

    feat: add new tools for routes, appdata and changelog (#7)

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
+import { umiAppdataList } from './tools/umi-appdata-list';
 import { umiBuild } from './tools/umi-build';
+import { umiChangelog } from './tools/umi-changelog';
 import { umiConfig } from './tools/umi-config';
 import { umiGenerate } from './tools/umi-generate';
 import { umiHelp } from './tools/umi-help';
 import { umiLint } from './tools/umi-lint';
 import { umiPlugin } from './tools/umi-plugin';
+import { umiRouteList } from './tools/umi-route-list';
 import { umiSetup } from './tools/umi-setup';
 import { umiVersion } from './tools/umi-version';
 import { ToolContext } from './types';
@@ -19,4 +22,7 @@ export function registerTools(toolContext: ToolContext) {
   umiPlugin(toolContext);
   umiGenerate(toolContext);
   umiLint(toolContext);
+  umiRouteList(toolContext);
+  umiAppdataList(toolContext);
+  umiChangelog(toolContext);
 }
---
+++ src/tools/umi-changelog.ts
@@ -0,0 +1,24 @@
+import { z } from 'zod';
+import { ToolContext } from '../types';
+
+export const umiChangelog = async ({ server }: ToolContext) => {
+  server.addTool({
+    name: 'umi-changelog',
+    description: 'Show the changelog of the  umi and @umijs/max.',
+    parameters: z.object({
+      version: z
+        .string()
+        .describe('Get the changelog for the current version,eg: 4.4.6'),
+    }),
+    execute: async ({ version }) => {
+      const result = await fetch(
+        `https://api.github.com/repos/umijs/umi/releases/tags/v${version}`,
+      );
+      if (!result.ok) {
+        throw new Error(`Failed to fetch changelog: ${result.statusText}`);
+      }
+      const { body } = await result.json();
+      return body;
+    },
+  });
+};
